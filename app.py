62872c940234a66b1ac7f70bf0ec4be999617e2a
turnkeyz <34150538+turnkeyz@users.noreply.github.com>
2021-09-26 18:43:40 -0500

    first test push to my branch to do a pull request. some changes are here.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] =''
@@ -15,9 +15,20 @@ def login():
 def home():
     return 'home'
 
-@app.route('/students')
+#defining a route for students page with post and get methods. Delete method will be added later on but this is basis.
+@app.route('/students', methods=['POST','GET'])
 def students():
-    return 'students'
+    #defining of error as temp holder for page response in case it is needed. this can be taken out if error testing is not required later on.
+    error = None
+    #if a get request then it returns a basic students.html page
+    if request.method!='GET':
+        return render_template('students.html')
+    #else it will return error and print out a status code
+    else:
+        #template text showcasing an error or something in else in the future. will return an error page or something.
+        return '<h1> Error with retrieval.</>'
+
+
 
 @app.route('/staff')
 def staff():
