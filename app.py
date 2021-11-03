53e3802894124aa003811925ae2c5df4eb4bb6fb
Eric Hoang <60450127+hoang200113@users.noreply.github.com>
2021-11-02 19:27:18 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,79 +1,38 @@
-from datetime import datetime, timedelta
-from flask import Flask, render_template, url_for, request, flash
-from flask.helpers import make_response
-import jwt
-import functools
-
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
-app.config['JWT_KEY'] ='soiqwueho28973987265362#^$%#'
-
-def secure_site(f):
-    @functools.wraps(f)
-    def secure_wrapper(*args, **kwargs):
-
-        token = request.cookies.get('token')
-        
-        if not token:
-            return "No token provided."
-        
-        try:
-            auth_data = jwt.decode(token, app.config['JWT_KEY'], algorithms=["HS256"])
-            print(auth_data)
-        except:
-            return "Token invalid."
-
-        return f(*args, **kwargs, auth_data = auth_data)
-    return secure_wrapper
-
+app.config['SECRET_KEY'] =''
 
 @app.route('/')
 def index():
-    return 'Hello, world!'
+    return 'Hello'
 
-@app.route('/login', methods = ['GET', 'POST'])
+@app.route('/login')
 def login():
-    if request.method == "GET":
-        return render_template("login.html")
-    elif request.method == "POST":
-        username = request.form['username']
-        password = request.form['password']
-
-        if username == "Braian" and password == "password":
-            token = jwt.encode({"username":username, "exp":datetime.utcnow() + timedelta(hours = 24)}, app.config['JWT_KEY'])
-            response = make_response(render_template("layout.html"))
-            response.set_cookie("token", token.encode("UTF-8"))
-            return response
-
-@app.route('/logout')
-def logout():
-    return 'Logged out.'
+    return 'world'
 
 @app.route('/home')
-@secure_site
-def home(auth_data = None):
-    return f"{auth_data['username']} you are logged in!"
+def home():
+    return 'home'
 
-# students page with diff request methods.
-# tables will be shown with editing functions add/edit/delete/etc.
-# data entered will be replaced with sql information once DB is up and running.
-@app.route('/students', methods=['POST','GET', 'DELETE', 'PUT'])
+#defining a route for students page with post and get methods. Delete method will be added later on but this is basis.
+@app.route('/students', methods=['POST','GET'])d
 def students():
+    #defining of error as temp holder for page response in case it is needed. this can be taken out if error testing is not required later on.
+    error = None
+    #if a get request then it returns a basic students.html page
     if request.method=='GET':
-        return render_template('students.html', studentName='John Doe', studentID='0001', subjects='Sample Text',
-                               grades='Sample Text', status='Active')
+        return render_template('students.html')
+    #else it will return error and print out a status code
     else:
         #template text showcasing an error or something in else in the future. will return an error page or something.
-        return render_template('error.html', studentName='John Doe')
+        return '<h1> Error with retrieval.</>'
+
 
 
 @app.route('/staff')
 def staff():
     return 'staff'
-
-@app.route('/portal')
-def portal():
-    return render_template('staffportal.html')
 
 """
 Debug mode to run the code without having to
@@ -81,4 +40,4 @@ run it from the terminal/cmd. Please remove it during
 production.
 """
 if __name__=='__main__':
-    app.run(debug=True)
+    app.run(debug=False)
