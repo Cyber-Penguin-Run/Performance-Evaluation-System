a7c6a23f62670257c5440a07658f3d481a08c259
BraianPita <braianx100@gmail.com>
2021-11-07 22:19:56 -0600

    improved dynamic navbar sections

--- admin_dashboard.py
+++ admin_dashboard.py
@@ -3,7 +3,9 @@ from flask.helpers import make_response
 from connection import Database
 from __main__ import app, secure_site, db
 
+nav_columns = {"Overview":"/admin/overview", "Staff":"/admin/staff", "Families":"/admin/families", "Business":"/admin/business"}
+
 @app.route("/admin/staff", methods = ["POST", "GET", "PUT", "DELETE"])
 @secure_site
 def admin_staff(auth_data = None):
-    return render_template('admin.html',auth_data=auth_data)
+    return render_template('admin.html',auth_data=auth_data, nav_columns = nav_columns)
--- my_student.py
+++ my_student.py
@@ -3,10 +3,12 @@ from flask.helpers import make_response
 from connection import Database
 from __main__ import app, secure_site, db
 
+nav_columns = {"Overview":"/mystudent/overview", "Students":"/mystudent/students", "Family":"/mystudent/family"}
+
 @app.route("/mystudent/overview", methods = ["POST", "GET", "PUT", "DELETE"])
 @secure_site
 def mystudent_overview(auth_data = None):
-    return render_template("")
+    return render_template("", nav_columns = nav_columns)
 
 @app.route("/mystudent/students", methods = ["POST", "GET", "PUT", "DELETE"])
 @secure_site
