5af0dbb9a407f6d5259c894505077ddb45507617
BraianPita <braianx100@gmail.com>
2021-11-07 18:10:18 -0600

    added routes to modules

--- coach_dashboard.py
+++ coach_dashboard.py
@@ -2,3 +2,13 @@ from flask import Flask, json, render_template, url_for, request, redirect, json
 from flask.helpers import make_response
 from connection import Database
 from __main__ import app, secure_site, db
+
+@app.route("/coach/home", methods = ["POST", "GET", "PUT", "DELETE"])
+@secure_site
+def coach_home(auth_data = None):
+    return "/coach/home"
+
+@app.route("/coach/info", methods = ["POST", "GET", "PUT", "DELETE"])
+@secure_site
+def coach_info(auth_data = None):
+    return "/coach/info"
--- testprep_dashboard.py
+++ testprep_dashboard.py
@@ -2,3 +2,8 @@ from flask import Flask, json, render_template, url_for, request, redirect, json
 from flask.helpers import make_response
 from connection import Database
 from __main__ import app, secure_site, db
+
+@app.route("/testprep", methods = ["POST", "GET", "PUT", "DELETE"])
+@secure_site
+def testprep(auth_data = None):
+    return "/testprep"
