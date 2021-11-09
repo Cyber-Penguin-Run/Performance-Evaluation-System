b8c1ba30621e93ab4393cf0fe9fb61fc920960b3
turnkeyz <34150538+turnkeyz@users.noreply.github.com>
2021-11-09 16:14:12 -0600

    updates to test prep navbar and pages and stuff

--- testprep_dashboard.py
+++ testprep_dashboard.py
@@ -3,9 +3,30 @@ from flask.helpers import make_response
 from connection import Database
 from __main__ import app, secure_site, db
 
-nav_columns = {}
+nav_columns = {'Mock ACT':'mockACT','Mock SAT':'mockSAT','Mock HSPT':'mockHSPT','Mock ISEE':'mockISEE'}
 
 @app.route("/testprep", methods = ["POST", "GET", "PUT", "DELETE"])
 @secure_site
 def testprep(auth_data = None):
     return render_template('test_prep_dashboard.html', auth_data = auth_data, nav_columns=nav_columns)
+
+@app.route("/testprep/mockACT", methods = ["POST", "GET", "PUT", "DELETE"])
+@secure_site
+def mockACT(auth_data = None):
+    return render_template('mockActScores.html', auth_data = auth_data, nav_columns=nav_columns)
+
+@app.route("/testprep/mockSAT", methods = ["POST", "GET", "PUT", "DELETE"])
+@secure_site
+def mockSAT(auth_data = None):
+    return render_template('mockSatScores.html', auth_data = auth_data, nav_columns=nav_columns)
+
+@app.route("/testprep/mockHSPT", methods = ["POST", "GET", "PUT", "DELETE"])
+@secure_site
+def mockHSPT(auth_data = None):
+    return render_template('mockHsptScores.html', auth_data = auth_data, nav_columns=nav_columns)
+
+@app.route("/testprep/mockISEE", methods = ["POST", "GET", "PUT", "DELETE"])
+@secure_site
+def mockISEE(auth_data = None):
+    return render_template('mockIseeScores.html', auth_data = auth_data, nav_columns=nav_columns)
+
