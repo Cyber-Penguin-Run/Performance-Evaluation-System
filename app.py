21d27cccbe35b2ac22640679bc4ece7623c20803
Ruben Guerra <70193006+Rubenthecat@users.noreply.github.com>
2021-11-11 03:46:23 -0600

    my current coach dashboard work

--- app.py
+++ app.py
@@ -138,12 +138,6 @@ def students():
         return render_template('error.html', studentName='John Doe')
 
 # redirect to /assignments to display table
-@app.route('/assignments')
-def assignments(auth_data = None):
-    nav_columns = {"Overview":"admin_overview", "Staff":"admin_staff", "Families":"admin_families", "Business":"admin_business"}
-
-    assignment_result = db.query(sql='SELECT* FROM assignments')
-    return render_template('assignments.html', assignments=assignment_result, auth_data=auth_data, nav_columns=nav_columns)
 
 # redirect to /sessions to display table
 @app.route('/sessions')
